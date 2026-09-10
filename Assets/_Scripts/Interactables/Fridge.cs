b0e9e3b87c0d4013cbdd74a91f5ba5a693d4ca81
Pedro Henriquez <pedrohenriiquez.2008@gmail.com>
2026-09-10 16:10:43 -0300

    Added fridge interaction

---
+++ Assets/_Scripts/Interactables/Fridge.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Fridge : MonoBehaviour, InteractibleInterface
+{
+    [SerializeField] private GameObject openFridge;
+    [SerializeField] private Vector3 prefabPosition;
+    [SerializeField] private Quaternion prefabRotation;
+    public void Interact()
+    {
+        GameObject clone;
+        clone = Instantiate(openFridge, prefabPosition, prefabRotation);
+        Destroy(gameObject);
+    }
+}
